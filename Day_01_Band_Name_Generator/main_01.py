a9d45d3b338441004cb59b8c358dde20fa31b880
LucasUandersonDev <lucasuanderson159@live.com>
2024-07-14 08:22:10 -0300

    Band Name Generator project
    - Implementei um script básico em Python para gerar nomes de bandas
    - Adicionei funcionalidade de entrada de usuário para cidade e nome do animal de estimação
    - Combinei as entradas para criar um nome de banda divertido

---
+++ Day_01_Band_Name_Generator/main_01.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Bem vindo ao projeto de nomes de bandas")
+#2. Ask the user for the city that they grew up in.
+city = input("Informe o nome da sua cidade\n")
+#3. Ask the user for the name of a pet.
+pet = input("Informe o nome do seu pet\n")
+#4. Combine the name of their city and pet and show them band name.
+print(f"O nome da sua banda poderia ser,{city}{pet}")
+#5; Make sure the input cursor shows on a new line, see the example at:
